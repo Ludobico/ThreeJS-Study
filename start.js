8f752dedaf1d13f7508174e772f8297bb5113996
김찬진 <aqs450@gmail.com>
2022-11-07 12:12:04 +0900

    ThreeJS starting menu commit

--- start.js
+++ start.js
@@ -1,27 +1,43 @@
+//scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
+
+//camera
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-const renderer = new THREE.WebGLRenderer();
+camera.position.set(0, 0, 3);
 
+//renderer
+const renderer = new THREE.WebGLRenderer({
+  //안티엘리어싱
+  antialias: true,
+  //아무것도 없는 공간은 투명하게
+  alpha: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+//mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ffff });
-
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-
-camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  cube.rotation.x -= 0.01;
   cube.rotation.y += 0.01;
 }
 animate();
+
+// 반응형 처리
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", onWindowResize);
